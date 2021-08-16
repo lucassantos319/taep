d59418c68b6dd14e572b5c7a2c6795962682b497
lucas <lucas.sidnei32@gmail.com>
2021-08-16 16:26:12 -0300

    teste

--- pages/sair.js
+++ pages/sair.js
@@ -3,21 +3,7 @@ import { useCookies } from 'react-cookie';
 
 const Sair = ({}) => {
 
-	const [userCookie, setCookie,removeCookies] = useCookies(["user"]);
-	const router = useRouter()
-
-	if ( Object.keys(userCookie).length !== 0 ){
-		const userInfoLogin= userCookie.user.login;
-		if ( userInfoLogin ){
-            removeCookies("user");
-            router.push("/");
-			return (<></>)	
-		}
-		
-	}
-    router.push("/");
-    return (<></>)	
-	
+	return (<><h2>saindo</h2></>)
 }
 
 
