396c8b4866d2297fba56ea45523027e67cb70330
Lucas Santos <lucas.sidnei32@gmail.com>
2021-10-02 20:11:21 -0300

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { CookiesProvider } from "react-cookie";
-
 function MyApp({ Component, pageProps }) {
   return(
-    <CookiesProvider>
       <Component {...pageProps} />
-    </CookiesProvider>
   ); 
 }
 
