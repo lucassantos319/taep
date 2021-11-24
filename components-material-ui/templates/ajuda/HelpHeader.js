2ce5a205c917f3abb9d9c6c991220165640506d6
Lucas Santos <lucas.sidnei32@gmail.com>
2021-11-24 19:38:21 -0300

    Change the background image

--- components-material-ui/templates/ajuda/HelpHeader.js
+++ components-material-ui/templates/ajuda/HelpHeader.js
@@ -2,8 +2,8 @@
 const HelpHeader = ({title}) => {
     return (
         <> 
-            <div >
-                <div style={{width:'100%',display:"flex",justifyContent:"center",alignItems:"center",height:"200px",backgroundSize:"cover",backgroundPosition:"center center",backgroundImage:"url(https://lh3.googleusercontent.com/0GalXUQ7XfYwMPo-yyNaPDlYkduMF06kYfBjbEx7QB-E7g9p_Wy6CK21nHRyrfccCjoZnQ=w16383)"}}>
+            <div style={{backgroundSize:"cover",backgroundPosition:"center center",backgroundImage:'url("/img/helpBackGround.jpg")'}}>
+                <div style={{width:'100%',display:"flex",justifyContent:"center",alignItems:"center",height:"200px", }}>
                     <h2 style={{color:"white"}}>{title}</h2>
                 </div>
             </div>
